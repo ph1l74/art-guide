bb316bea42cddc6725841ee92fee9c726f63c3f7
Filat Astakhov <astakhovfilat@gmail.com>
2019-09-10 13:55:40 +0500

    Updates

---
+++ precache-manifest.77d6ba6075076ee338de236820f59b7f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69e0ac4cb226a15d3ba576d0f3ef484",
+    "url": "/art-guide/index.html"
+  },
+  {
+    "revision": "9244d670a895850dc91a",
+    "url": "/art-guide/static/css/main.5ff2a1d9.chunk.css"
+  },
+  {
+    "revision": "90250ca821752330a52a",
+    "url": "/art-guide/static/js/2.5f4bf150.chunk.js"
+  },
+  {
+    "revision": "9244d670a895850dc91a",
+    "url": "/art-guide/static/js/main.1f76a392.chunk.js"
+  },
+  {
+    "revision": "840e4db7628b97278fef",
+    "url": "/art-guide/static/js/runtime~main.cb0ef090.js"
+  },
+  {
+    "revision": "83d37d2cd8867370b41a9b50408e924f",
+    "url": "/art-guide/static/media/admarginem-logo.83d37d2c.png"
+  },
+  {
+    "revision": "99b84530ad4915be6991f5345c438985",
+    "url": "/art-guide/static/media/afisha-logo.99b84530.png"
+  },
+  {
+    "revision": "944dca6db2d9e1e8588a078ec3571b53",
+    "url": "/art-guide/static/media/archiru-logo.944dca6d.jpg"
+  },
+  {
+    "revision": "bd4ec96eba6c680adce9c5b12fa0c54a",
+    "url": "/art-guide/static/media/askmen-logo.bd4ec96e.jpg"
+  },
+  {
+    "revision": "fb4cef2b134d83e3d2d409721b9e0660",
+    "url": "/art-guide/static/media/birdinflight-logo.fb4cef2b.png"
+  },
+  {
+    "revision": "3a0134e5484b2bc447f9a3f75896a42b",
+    "url": "/art-guide/static/media/losko-logo.3a0134e5.jpg"
+  },
+  {
+    "revision": "75b8531f2a3ec7e5a9277fbdc35c3951",
+    "url": "/art-guide/static/media/mif-logo.75b8531f.svg"
+  },
+  {
+    "revision": "6e9f304c1900646268a3185fd0901c15",
+    "url": "/art-guide/static/media/postnauka-logo.6e9f304c.png"
+  },
+  {
+    "revision": "0c0455df6910c49964419e56c5e3a553",
+    "url": "/art-guide/static/media/pozitiv-logo.0c0455df.png"
+  },
+  {
+    "revision": "4f709acdee6b7f7854f91d3bb75061bf",
+    "url": "/art-guide/static/media/spletnik-logo.4f709acd.svg"
+  },
+  {
+    "revision": "3c3505ef594cc32af5f661c5fc0b6ec9",
+    "url": "/art-guide/static/media/strelkamag-logo.3c3505ef.jpg"
+  },
+  {
+    "revision": "ab84352691898779b271507307428b08",
+    "url": "/art-guide/static/media/tanp-logo.ab843526.png"
+  },
+  {
+    "revision": "0ecd10ea775e185dc18161e37912fdb7",
+    "url": "/art-guide/static/media/tatlin-logo.0ecd10ea.jpg"
+  }
+]);
